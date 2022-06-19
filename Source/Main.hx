793d0f26b9cb7ec86df864fcb66b9fa48c0266fd
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2022-06-19 23:21:54 +0300

    bruh neko and hl dont wanna work

--- Source/Main.hx
+++ Source/Main.hx
@@ -7,6 +7,12 @@ class Main extends Sprite
 	
 	public function new()
 	{
+		final args = Sys.args();
+		for (arg in args) {
+			switch arg {
+				case "-justUpdate": Sys.exit(0);
+			}
+		}
 		super();
 	}
 }

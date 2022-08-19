3a2b0b9f062ee546f8be3159c9f6357a46790db6
ShaharMS <shaharmsecond@gmail.com>
2022-08-19 14:57:30 +0300

    renamed AutoUpdater -> Updater, removed manual updates

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,23 +1,21 @@
 package;
 
-
-import openfl.system.Capabilities;
-import sys.io.Process;
-import sys.io.File;
-import sys.FileSystem;
-import openfl.display.Sprite;
-import openfl.text.TextFormat;
 import haxe.ui.Toolkit;
-class Main extends Sprite
-{
+import openfl.display.Sprite;
+import openfl.system.Capabilities;
+import openfl.text.TextFormat;
+import sys.FileSystem;
+import sys.io.File;
+import sys.io.Process;
+
+class Main extends Sprite {
 	public static var TEST:Bool = false;
 
 	public static var mode(default, null):InstallerMode = MANUAL;
 
 	public static final textFormat:TextFormat = new TextFormat(fontName, fontSize, fontColor);
-	
-	public function new()
-	{
+
+	public function new() {
 		Toolkit.init();
 		Toolkit.theme = "dark";
 		app.window.focus();
@@ -38,27 +36,33 @@ class Main extends Sprite
 		final args = Sys.args();
 		for (arg in args) {
 			switch arg {
-				case "-manualUpdate": mode = UPDATE;
-				case "-autoUpdate": mode = AUTOUPDATE;
-				case "-firstInstall" | "-firstRun" | "-first": mode = INSTALL;
-				case "-uninstall": mode = UNINSTALL;
-				case "-regular" | "-manual": mode = MANUAL;
-				case "-test": TEST = true;
+				case "-autoUpdate" | "-update":
+					mode = UPDATE;
+				case "-firstInstall" | "-firstRun" | "-first":
+					mode = INSTALL;
+				case "-uninstall":
+					mode = UNINSTALL;
+				case "-regular" | "-manual":
+					mode = MANUAL;
+				case "-test":
+					TEST = true;
 			}
 		}
 		super();
 		switch mode {
-			case INSTALL: addChild(new Installer());
-			case UNINSTALL: addChild(new UnInstaller());
-			case UPDATE: addChild(new Updater());
-			case AUTOUPDATE: addChild(new AutoUpdater());
-			case MANUAL: addChild(new Menu());
+			case INSTALL:
+				addChild(new Installer());
+			case UNINSTALL:
+				addChild(new UnInstaller());
+			case UPDATE:
+				addChild(new Updater());
+			case MANUAL:
+				addChild(new Menu());
 		}
 	}
 }
 
 enum InstallerMode {
-	AUTOUPDATE;
 	UPDATE;
 	INSTALL;
 	UNINSTALL;
